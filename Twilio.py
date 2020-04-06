680fcf4e69b6d21c8608d06422224cb2638b85e8
clineblue <growl7617@naver.com>
2020-04-06 12:36:26 +0900

    Twilio Open Api를 이용해서 내 번호로 문자 받아보기.

--- Twilio.py
+++ Twilio.py
@@ -3,15 +3,15 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC61f682127809466145c8d416aed0ae6e'
-auth_token = 'be7288eb387df031d7b61ff6417c8790'
+account_sid = '홈페이지에서 받은 시드값'
+auth_token = '홈페이지에서 받은 토큰 값'
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
-                     body="Hi. this is text message test.",
+                     body="보낼 메시지",
                      from_='+14439917448',
-                     to='+821035337121'
+                     to='+82전화번호'
                  )
 
 print(message.sid)
