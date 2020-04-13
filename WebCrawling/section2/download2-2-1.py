873c46860fed4a4d1f8fe5d1173d979d1a049f45
clineblue <growl7617@naver.com>
2020-04-14 00:19:25 +0900

    urllib 활용한 웹에서 필요 데이터 추출

---
+++ WebCrawling/section2/download2-2-1.py
@@ -0,0 +1,14 @@
+import sys
+import io
+import urllib.request as dw
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+imgUrl = "http://shop1.phinf.naver.net/20200318_269/15845006823959Vrd0_JPEG/21860516940677921_22371427.jpg"
+htmlUrl = "http://www.google.com"
+savePath1 = "/Users/kim/Desktop/test1.jpg"
+savePath2 = "/Users/kim/Desktop/index.html"
+dw.urlretrieve(imgUrl, savePath1)
+dw.urlretrieve(htmlUrl, savePath2)
+print("다운로드 완료")
---
+++ WebCrawling/section2/download2-2-2.py
@@ -0,0 +1,23 @@
+import sys
+import io
+import urllib.request as dw
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
+imgUrl = "http://shop1.phinf.naver.net/20200318_269/15845006823959Vrd0_JPEG/21860516940677921_22371427.jpg"
+htmlUrl = "http://www.google.com"
+savePath1 = "/Users/kim/Desktop/test1.jpg"
+savePath2 = "/Users/kim/Desktop/index.html"
+
+f = dw.urlopen(imgUrl).read()
+f2 = dw.urlopen(htmlUrl).read()
+
+saveFile1 = open(savePath1, 'wb')
+saveFile1.write(f)
+saveFile1.close()
+
+with open(savePath2, 'wb') as saveFile2:
+    saveFile2.write(f2)
+
+print("다운로드 완료")
