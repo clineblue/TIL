9c8107cb25c18826f17ca40306462f341211e6e5
clineblue <growl7617@naver.com>
2020-07-01 17:51:26 +0900

    네이버 이미지 스크랩핑

--- WebCrawling/section2/2-8-1.py
+++ WebCrawling/section2/2-8-1.py
@@ -1,4 +1,4 @@
-#네이버 원하는 이미지 다운받기
+#네이버 이미지 다운받기
 from bs4 import BeautifulSoup
 import urllib.request as req
 import urllib.parse as rep
@@ -23,6 +23,7 @@ except OSError as e:
     if e.errno != errno.EEXIST:
         print("Failed to create directory")
         raise
+# 폴더 생성
 # try:
 #     if not(os.path.isdir(savePath)):
 #         os.makedirs(os.path.join(savePath))
@@ -30,12 +31,15 @@ except OSError as e:
 #     if e.errno != errno.EEXIST:
 #         print("Failed to create directory!")
 #         raise
+# 폴더 생성.v2
 
 soup = BeautifulSoup(res, "html.parser")
 li_list = soup.select("div > a.thumb._thumb > img")
+#print(soup)
 
 for i, e in enumerate(li_list,1):
     fileName = os.path.join(savePath, savePath+str(i)+'.jpg')
     req.urlretrieve(e['data-source'], fileName)
+#user-agent가 브라우저가 아니므로 img src가 아닌 data-source가 이미지의 출처로 나옴
 
 print("다운로드 완료!")
